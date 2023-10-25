d55aeb0e45e0fc9d83414686ab39b08eb9cb8059
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2023-10-25 20:36:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+var time=20;
+var score=0;
+var hitno=0;
+function makebubble(){
+  var clutter="";
+  for(i=0;i<120;i++){
+      var random= Math.floor(Math.random()*10);
+    clutter += ` <div id="bubble">${random}</div>`
+  }
+  document.querySelector("#panelbottom").innerHTML=clutter;
+
+}
+makebubble();
+function runtimer(){
+var abc=setInterval(function(){
+ if(time>0){
+  if(time>10){ 
+    time--;
+    document.querySelector("#timer").innerHTML=time
+    document.querySelector("#timer").style.color="green"
+    document.querySelector("#timer").style.backgroundColor="bisque"
+
+  }
+  else if(0<time<10){
+    
+    time--;
+    document.querySelector("#timer").innerHTML=time
+    document.querySelector("#timer").style.color="red"
+    document.querySelector("#timer").style.backgroundColor="black"
+
+
+  }
+ }
+
+  else{
+    clearInterval(abc);
+    document.querySelector("#panelbottom").innerHTML=`<h1 id="result">Your Score is ${score} <h1>`
+  }
+  
+
+
+}, 1000)
+
+}
+runtimer();
+function hitscore(){
+hitno=Math.floor(Math.random()*10);
+document.querySelector("#hit").textContent=hitno;
+}
+hitscore();
+function scoreincrese(){
+  score=score+10;
+  document.querySelector("#score").textContent=score;
+}
+
+function clickbubble(){
+var a=document.querySelector("#panelbottom");
+a.addEventListener("click", function(abcd){
+  var clickednum= Number(abcd.target.textContent);
+if(clickednum==hitno){
+  scoreincrese();
+  makebubble();
+  hitscore();
+
+}
+})
+}
+clickbubble();
