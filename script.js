3330228a767fa72d165d688c13c34a9d0885a2ce
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2023-11-29 20:07:03 +0530

    play again button added

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ function makebubble(){
   document.querySelector("#panelbottom").innerHTML=clutter;
 
 } 
-makebubble(); 
+makebubble();  
 function runtimer(){
 var abc=setInterval(function(){
  if(time>0){
   if(time>10){ 
     time--;
     document.querySelector("#timer").innerHTML=time
-    document.querySelector("#timer").style.color="green"
-
+    document.querySelector("#timer").style.color=" color: rgb(0, 185, 0)";
+  
   }
   else if(0<time<10){
     
@@ -27,10 +27,13 @@ var abc=setInterval(function(){
     document.querySelector("#timer").style.color="red"
   }
  }
-  else{
-    clearInterval(abc);
-    document.querySelector("#panelbottom").innerHTML=`<h1 id="result">Your Score is ${score} <h1>`
-  }
+ else {
+  clearInterval(abc);
+  document.querySelector("#panelbottom").innerHTML = `
+    <div id="resultdisplay"> <h1 id="result" style="background-color: none;">Your Score is ${score}</h1>
+    <button onclick="playagain();" id="btn" class="playagainbtn">PLAY AGAIN</button></div>
+  `;
+}
 }, 1000)
 
 }
@@ -59,23 +62,25 @@ if(clickednum==hitno){
 }
 clickbubble();
 
+var flag = 1;
 
-var flag=1;
-function abc(){
-  
-var x= document.querySelector("#panelbottom");
-var y = document.querySelector("#bubble");
-var z = document.querySelector("#btn");
-if(flag==1){
-  x.style.backgroundColor=" rgb(75, 75, 75)";
-z.innerHTML="DARK MODE"
-
-flag=0;
+function lightDarkMode() {
+  var x = document.querySelector("#panelbottom");
+  var z = document.querySelector("#btn");
+  if (flag === 1) {
+    x.style.backgroundImage = "linear-gradient(to right, #767676, #284357)";
+    z.innerHTML = "DARK MODE";
+    flag = 0;
+  } else {
+    x.style.backgroundImage = "linear-gradient(to right, #131f2d, #081122)";
+    z.innerHTML = "LIGHT MODE";
+    flag = 1;
+  }
 }
-else{
-  x.style.backgroundColor=" rgb(10, 10, 10)";
-  z.innerHTML="LIGHT MODE"
 
-flag=1;
-}
+function playagain(){
+
+setTimeout(function() {
+  location.reload();
+}, 250);
 }
